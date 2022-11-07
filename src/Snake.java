4b0a113fcd494e6d7d78375e18cc597e349a6df5
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-07 11:03:33 +0100

    Création de la class Snake

---
+++ src/Snake.java
@@ -0,0 +1,12 @@
+import utils.AgentAction;
+import utils.ColorSnake;
+import utils.FeaturesSnake;
+import utils.Position;
+
+import java.util.ArrayList;
+
+public class Snake extends FeaturesSnake {
+    public Snake(ArrayList<Position> positions, AgentAction lastAction, ColorSnake colorSnake, boolean isInvincible, boolean isSick) {
+        super(positions, lastAction, colorSnake, isInvincible, isSick);
+    }
+}
