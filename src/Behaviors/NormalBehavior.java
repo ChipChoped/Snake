ae155a96c3a3a9fe2198efaa6d506707f28c1d95
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-15 14:56:18 +0100

    Create NormalBehavior.java

---
+++ src/Behaviors/NormalBehavior.java
@@ -0,0 +1,15 @@
+package Behaviors;
+
+import utils.Position;
+import java.util.ArrayList;
+import utils.Snake;
+
+public class NormalBehavior implements Behavior {
+    public boolean isLegalMove(Snake snake, ArrayList<Position> positions) {
+        return false;
+    }
+
+    public void moveAgent(Snake snake, ArrayList<Position> positions) {
+
+    }
+}
