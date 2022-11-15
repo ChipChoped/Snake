bb83d6465e83bec48cdbd8982556f1ede66e53d2
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-15 14:56:14 +0100

    Create Behavior.java

---
+++ src/Behaviors/Behavior.java
@@ -0,0 +1,10 @@
+package Behaviors;
+
+import utils.Position;
+import java.util.ArrayList;
+import utils.Snake;
+
+public interface Behavior {
+    public boolean isLegalMove(Snake snake, ArrayList<Position> positions);
+    public void moveAgent(Snake snake, ArrayList<Position> positions);
+}
