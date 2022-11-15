b21b45467767559101f15493642fa54aa279a6e1
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-15 15:11:20 +0100

    Def of isLegal method

--- src/behaviors/NormalBehavior.java
+++ src/behaviors/NormalBehavior.java
@@ -5,7 +5,10 @@ import utils.Snake;
 
 public class NormalBehavior implements Behavior {
     public boolean isLegalMove(Snake snake, AgentAction action) {
-        return false;
+        return (action == AgentAction.MOVE_UP && snake.getLastAction() != AgentAction.MOVE_DOWN) ||
+                (action == AgentAction.MOVE_DOWN && snake.getLastAction() != AgentAction.MOVE_UP) ||
+                (action == AgentAction.MOVE_LEFT && snake.getLastAction() != AgentAction.MOVE_RIGHT ||
+                (action == AgentAction.MOVE_RIGHT && snake.getLastAction() != AgentAction.MOVE_LEFT));
     }
 
     public void moveAgent(Snake snake, AgentAction action) {
