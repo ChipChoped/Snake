a175a4ce12405b6d5e918ac9150a1e5c953a4ac3
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-15 17:22:14 +0100

    Display of snakes movements

--- src/ViewSnakeGame.java
+++ src/ViewSnakeGame.java
@@ -8,12 +8,14 @@ import java.util.Observer;
 
 public class ViewSnakeGame implements Observer {
     protected AbstractController controller;
+    protected PanelSnakeGame panelSnakeGame;
+    JFrame frame = new JFrame();
 
     public ViewSnakeGame(Observable obs, AbstractController controller, PanelSnakeGame panelSnakeGame) {
         obs.addObserver(this);
         this.controller = controller;
+        this.panelSnakeGame = panelSnakeGame;
 
-        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Snake");
         frame.setSize(new Dimension(700, 700));
@@ -27,14 +29,16 @@ public class ViewSnakeGame implements Observer {
         frame.setLocation(dx,dy);
 
         try {
-            frame.add(panelSnakeGame);
+            frame.add(this.panelSnakeGame);
         }
         catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
     }
-
+    
     public void update(Observable observable, Object o) {
-        SimpleGame game = (SimpleGame) o;
+        SnakeGame game = (SnakeGame) observable;
+        this.panelSnakeGame.updateInfoGame(game.getFeaturesSnakes(), game.getItems());
+        frame.repaint();
     }
 }
