073046e325fef4bb82a79b141ee2d083c758d492
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-15 15:49:13 +0100

    behavior attribut added

--- src/utils/Snake.java
+++ src/utils/Snake.java
@@ -1,18 +1,20 @@
 package utils;
 
-import utils.AgentAction;
-import utils.ColorSnake;
-import utils.FeaturesSnake;
-import utils.Position;
+import behaviors.Behavior;
+import behaviors.NormalBehavior;
 
 import java.util.ArrayList;
 
 public class Snake extends FeaturesSnake {
+    Behavior behavior;
+
     public Snake(ArrayList<Position> positions, AgentAction lastAction, ColorSnake colorSnake, boolean isInvincible, boolean isSick) {
         super(positions, lastAction, colorSnake, isInvincible, isSick);
+        behavior = new NormalBehavior();
     }
 
     public Snake(FeaturesSnake snake) {
         super(snake.getPositions(), snake.getLastAction(), snake.getColorSnake(), snake.isInvincible(), snake.isSick());
+        behavior = new NormalBehavior();
     }
 }
