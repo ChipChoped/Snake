f632e50e45f881875631cd4d12bb5b4eeabed63c
Chaïmaâ Touhami <chaimaa-touhami@outlook.fr>
2022-11-28 09:52:33 +0100

    Added onBorder method

--- src/behaviors/NormalBehavior.java
+++ src/behaviors/NormalBehavior.java
@@ -14,9 +14,26 @@ public class NormalBehavior implements Behavior {
                 (action == AgentAction.MOVE_RIGHT && snake.getLastAction() != AgentAction.MOVE_LEFT));
     }
 
-    public boolean isEliminated(Snake snake, AgentAction action, ArrayList<Snake> otherSnakes, int sizeX, int sizeY, boolean withWalls) {
-        return false;
+    public boolean isEliminated(Snake snake, Position position, ArrayList<Snake> otherSnakes, int sizeX, int sizeY, boolean withWalls) {
+        if (withWalls)
+            if (position.getX() == 0 || position.getY() == 0 ||
+                position.getX() == sizeX - 1 || position.getY() == sizeY - 1) {
+                System.out.println("Wall");
+                return true;
+            }
 
+        for (Snake otherSnake : otherSnakes)
+            if (otherSnake.getPositions().contains(position)) {
+                System.out.println("Collision");
+                return true;
+            }
+
+        return false;
+    }
+
+    public boolean onBorder(Position position, int sizeX, int sizeY) {
+        return position.getX() == 0 || position.getY() == 0 ||
+                position.getX() == sizeX - 1 || position.getY() == sizeY - 1;
     }
 
     public void moveAgent(Snake snake, AgentAction action, ArrayList<Snake> otherSnakes, int sizeX, int sizeY, boolean withWalls) {
@@ -26,20 +43,29 @@ public class NormalBehavior implements Behavior {
             for (Position position : snake.getPositions())
                 positions.add(new Position(position));
 
+            int move[];
+
+            if (onBorder(positions.get(0), sizeX, sizeY))
+                move = new int[]{sizeX - 1, sizeY - 1};
+            else
+                move = new int[]{1, 1};
+
             switch (action) {
                 case MOVE_UP:
-                    positions.get(0).setY(positions.get(0).getY() - 1);
+                    positions.get(0).setY(positions.get(0).getY() - move[1]);
                     break;
                 case MOVE_DOWN:
-                    positions.get(0).setY(positions.get(0).getY() + 1);
+                    positions.get(0).setY(positions.get(0).getY() + move[1]);
                     break;
                 case MOVE_LEFT:
-                    positions.get(0).setX(positions.get(0).getX() - 1);
+                    positions.get(0).setX(positions.get(0).getX() - move[0]);
                     break;
                 case MOVE_RIGHT:
-                    positions.get(0).setX(positions.get(0).getX() + 1);
+                    positions.get(0).setX(positions.get(0).getX() + move[0]);
                     break;
             }
+
+            // isEliminated(snake, positions.get(0), otherSnakes, sizeX, sizeY, withWalls);
 
             snake.setPositions(positions);
         }
